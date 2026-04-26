50462c197f21fa059c5e65ac1209425938383011
Sokkhan099541111 <sokkhan.chhon@gmail.com>
2026-04-26 14:07:03 +0700

    Fix formatting in database connection configuration

--- src/database/db.mjs
+++ src/database/db.mjs
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
  host: process.env.DB_HOST || 'report.mangotracking.com',
  user: process.env.DB_USER || 'mangotracking_api',
  password: process.env.DB_PASSWORD || 'Sok@$#$2026',
-database: process.env.DB_NAME || 'mangotracking_api',
+ database: process.env.DB_NAME || 'mangotracking_api',
 });
   // host: process.env.DB_HOST || '74.48.72.98',
 export default connection;
