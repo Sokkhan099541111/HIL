dd8174ea64778d039a56b573bfe90939102736d0
Sokkhan099541111 <sokkhan.chhon@gmail.com>
2026-04-26 14:02:49 +0700

    Update db.mjs

--- src/database/db.mjs
+++ src/database/db.mjs
@@ -1,15 +1,18 @@
 import mysql from 'mysql2/promise';
 
 const connection = mysql.createPool({
-    host: process.env.DB_HOST || 'report.mangotracking.com',
-    user: process.env.DB_USER || 'mangotracking_api',
-    password: process.env.DB_PASSWORD || 'Sok@$#$2026',
-    database: process.env.DB_NAME || 'mangotracking_api',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
   // host: process.env.DB_HOST || '74.48.72.98',
 export default connection;
 
-
+ // host: process.env.DB_HOST || 'report.mangotracking.com',
+ //    user: process.env.DB_USER || 'mangotracking_api',
+ //    password: process.env.DB_PASSWORD || 'Sok@$#$2026',
+ //    database: process.env.DB_NAME || 'mangotracking_api',
 // import mysql from 'mysql2/promise';
 
 // const connection = mysql.createPool({
